0195c02b28fd64cec3f3eafbe41b42d7bbf3372f
Theomat <theomatricon@gmail.com>
2021-07-15 21:49:08 +0200

    Added simple enumeration test

---
+++ sbsur/tests/test_enumeration.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from sbsur import sample, SequenceGenerator
+
+import numpy as np
+import pytest
+
+# Test parameters
+DEPTH: int = 5
+CATEGORIES: int = 5
+TESTS: int = 10
+# Constants for test
+BATCH_SIZE : int = CATEGORIES
+SIZE: int = DEPTH * 5
+SAMPLES: int = int(CATEGORIES**DEPTH)
+testdata = [np.random.uniform(size=CATEGORIES*DEPTH) for _ in range(TESTS)]
+
+
+def make_logprobs_getter(probs: np.ndarray):
+    def get_logprobs(sequence: list[int]) -> Optional[list[float]]:
+        if len(sequence) == DEPTH:
+            return None
+        return np.log(probs[:, len(sequence)])
+    return get_logprobs
+
+@pytest.mark.parametrize("probabilities", testdata)
+def test_enumeration(probabilities: np.ndarray):
+    probabilities = probabilities.reshape((-1, DEPTH))
+    for i in range(DEPTH):
+        probabilities[:, i] /= np.sum(probabilities[:, i])
+
+    gen = SequenceGenerator(make_logprobs_getter(probabilities), CATEGORIES, 0)
+    results = []
+    for _ in range(SAMPLES // BATCH_SIZE):
+        sequences_list = sample(gen, BATCH_SIZE)
+        for s in sequences_list:
+            sequence = "".join([str(x) for x in s])
+            if sequence in results:
+                print("failed to sample unique sequence:", np.unique(results).shape[0], "/", SAMPLES)
+                assert False
+            results.append(sequence)
+    
