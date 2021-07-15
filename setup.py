4cfc5312545e21dc6edaa17693b15b3e64ec0f73
Theomat <theomatricon@gmail.com>
2021-07-15 13:20:33 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""Installs SBS+UR."""
+import setuptools
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+try:
+  import numpy
+except:
+  assert False, "Numpy needs to be installed for correct cython compilation!"
+
+
+def run_setup():
+  """Installs SBSUR."""
+
+  with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+  setuptools.setup(
+      name='sbsur',
+      version="0.0.1",
+      author='Théo Matricon, Nathanal Fijalkow',
+      author_email='theomatricon@gmail.com',
+      description='Stochastic Beam Search + UniqueRandomizer: Fast Incremental Sampling Without Replacement',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/Theomat/sbsur',
+      packages=setuptools.find_packages(),
+      install_requires=[
+        'numpy >= 1.15.4', # Update numpy version compatible with ptyhon 3.6
+        'cython'
+      ],
+      extras_require={'dev': ['pytest', 'scipy']},
+      python_requires='>=3.6',
+      ext_modules=cythonize("sbsur/*.pyx", include_path=[numpy.get_include()]),
+      zip_safe=False,
+      cmdclass = {'build_ext':build_ext}
+  )
+
+
+if __name__ == '__main__':
+  run_setup()
