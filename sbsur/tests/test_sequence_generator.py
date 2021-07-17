7e555b79d4515537a54adc31f4ad9a525ad9c390
Theomat <theomatricon@gmail.com>
2021-07-17 14:56:26 +0200

    Fixed test

--- sbsur/tests/test_sequence_generator.py
+++ sbsur/tests/test_sequence_generator.py
@@ -3,13 +3,13 @@ from sbsur import SequenceGenerator
 
 def test_with_seed():
     try:
-        seq_gen = SequenceGenerator(lambda _: None, 2, 0)
+        seq_gen = SequenceGenerator(lambda _: [1], 2, 0)
     except:
         assert False
 
 
-def test_with_nonee_seed():
+def test_with_none_seed():
     try:
-        seq_gen = SequenceGenerator(lambda _: None, 2, None)
+        seq_gen = SequenceGenerator(lambda _: [1], 2, None)
     except:
         assert False
