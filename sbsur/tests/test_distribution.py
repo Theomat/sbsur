f3f5f20c2c87a05543716bf348eca5e7afdd4fbb
Theomat <theomatricon@gmail.com>
2021-09-06 17:08:55 +0200

    Added extensive distribution test

--- sbsur/tests/test_distribution.py
+++ sbsur/tests/test_distribution.py
@@ -3,7 +3,8 @@ from sbsur import sample, SequenceGenerator
 
 import collections
 from math import log
-
+import numpy as np
+import pytest
 
 
 def get_logprobs(sequence: list[int]) -> Optional[list[float]]:
@@ -50,3 +51,54 @@ def test_proportions():
     # P('012') = P('102') = 0.2 * 0.25.
     assert abs(counter['012'] - 0.2 * 0.25 * 10000) <= 100
     assert abs(counter['102'] - 0.2 * 0.25 * 10000) <= 100
+
+
+# Test parameters
+# Normal approximation doesn't work well when probabilities are low
+# So use low depth
+DEPTH: int = 3
+CATEGORIES: int = 5
+TESTS: int = 10
+SAMPLES: int = int(1e5)
+np.random.seed(0)
+testdata = [np.random.uniform(size=CATEGORIES*DEPTH) for _ in range(TESTS)]
+
+
+def make_logprobs_getter(probs: np.ndarray):
+    def get_logprobs(sequence: list[int]) -> Optional[list[float]]:
+        if len(sequence) < len(probs):
+            return np.log(probs[len(sequence)])
+        return None
+    return lambda x: [get_logprobs(el) for el in x]
+
+@pytest.mark.parametrize("probabilities", testdata)
+def test_uniform_trees(probabilities: np.ndarray):
+    probabilities = probabilities.reshape((DEPTH, -1))
+    for i in range(DEPTH):
+        probabilities[i, :] /= np.sum(probabilities[i, :])
+
+
+    results = []
+    for i in range(SAMPLES):
+        gen = SequenceGenerator(make_logprobs_getter(probabilities), CATEGORIES, i)
+        sequences_list = sample(gen, 1)
+        results.append(tuple(sequences_list[0]))
+    counter = collections.Counter(results)
+    ci = 0.95
+    alpha = 1 - ci
+    z = 2 * (1 - alpha / 2)
+    total = len(counter)
+    failed = 0
+    for sequence, count in counter.items():
+        p = np.prod([probabilities[i, j] for i, j in enumerate(sequence)])
+        # count ~ Binomial(p, SAMPLES)
+        p_hat = count / SAMPLES
+        # 95% confidence interval
+        atol = z * np.sqrt(p_hat * (1 - p_hat) / SAMPLES) * SAMPLES
+        expected_count =  p * SAMPLES
+        failed += not np.isclose(count, expected_count, rtol=0, atol=atol)
+
+    # failed ~ Binomial(0.05, total)
+    p_hat = failed / total
+    max_failed = .05 * total + z * np.sqrt(p_hat * (1 - p_hat) / total) * total
+    assert failed <= max_failed
